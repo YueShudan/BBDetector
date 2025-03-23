7a4cab2304e319e9ab173292671764e36f600d62
dandan <103083413+YueShudan@users.noreply.github.com>
2025-03-23 11:57:05 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -17,7 +17,7 @@ column_names0 = rows_with_one0.iloc[:, 0]
 unique_column_names1 = set(column_names1)
 unique_column_names0 = set(column_names0)
 
-print("------------正负样本函数对应的程序-------------")
+print("------------Positive and negative sample function corresponding to the binaries-------------")
 print("unique_column_names1:", unique_column_names1)
 print("unique_column_names0:", unique_column_names0)
 
@@ -26,7 +26,7 @@ common_elements = set(unique_column_names0).intersection(set(unique_column_names
 
 unique_column_names0_modified = [elem for elem in unique_column_names0 if elem not in common_elements]
 
-print("------------在正样本中出现的程序，在负样本中删除-------------")
+print("------------binaries that appear in the positive sample are removed from the negative sample-------------")
 print("Pos_samples_list:", unique_column_names1)
 print("Pos_samples_len:", len(unique_column_names1))
 print("Neg_sample_list (modified):", unique_column_names0_modified)
@@ -38,5 +38,5 @@ with open('samples/DWR-932_Pre_best_model_Pos_samples.txt', 'w', encoding='utf-8
 with open('samples/DWR-932_Pre_best_model_Neg_sample.txt', 'w', encoding='utf-8') as file2:
     file2.write('\n'.join(unique_column_names0_modified))
 
-print("两个列表的内容已经写入到文件中。")
+print("The contents of both lists have been written to the file")
 
